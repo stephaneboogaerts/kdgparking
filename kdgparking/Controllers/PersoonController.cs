0868a4c90f3b5bdcbefacb65722bc8bf298b701d
work4bots <jossed.dewitte@gmail.com>
2019-02-17 13:48:43 +0100

    Site update naar versie Josse
    Signed-off-by: work4bots <jossed.dewitte@gmail.com>

---
+++ kdgparking/Controllers/PersoonController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace testParkingWeb.Controllers
+{
+    public class PersoonController : Controller
+    {
+        public ActionResult Toevoegen()
+        {
+            return View();
+        }
+    }
+}
